3721ce3d0dca1b3050533bfc3ede3fd8ddab837c
hoyes127 <stian.salte@hotmail.com>
2025-08-16 14:24:44 +0200

    big update

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from constants import *
+from player import Player
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        for draw in drawable:
+            draw.draw(screen)
+        updatable.update(dt)
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
